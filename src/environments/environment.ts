5ae6b38b1929ea917963826be9363535d2e89125
DESKTOP-9T6IOUK\deyvison <>
2025-11-20 13:25:12 -0300

    dasdasdas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://api_animal_adoption:8081/api',
-  apiUrlPublic: 'http://api_animal_adoption:8081/public',
+  apiUrl: '/api',
+  apiUrlPublic: '/api/public',
   keycloakConfig: {
     url: 'https://keycloak.barbershop-app.shop:8443',
     realm: 'ANIMAL_ADOPTION',
