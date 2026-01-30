744cef1f538731ca857472a3d3d7eb5471c618a9
Deyvison <tutacf@gmail.com>
2026-01-30 14:05:14 -0300

    Adicionado logo da empresa desevolvedora

--- src/app/layout/layout-admin/footer/footer.component.ts
+++ src/app/layout/layout-admin/footer/footer.component.ts
@@ -1,11 +1,14 @@
+import { UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, UpperCasePipe],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  companyName: string = 'DGO Developer';
+}
