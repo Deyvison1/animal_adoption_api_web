739350d157eaefd302e60cefcdeee70abfa9e5f2
DESKTOP-PNMASB0\computador <tutacf@gmail.com>
2026-08-23 14:35:34 -0300

    asdsa

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  apiUrl: '/api',
-  apiUrlPublic: '/public',
+  apiUrl: 'http://192.168.18.7:8083/api',
+  apiUrlPublic: 'http://192.168.18.7:8083/public',
   keycloakConfig: {
-    url: 'https://keycloak.animal-adoption.com.br:8443',
+    url: 'http://keycloak.animal-adoption.com.br:8080',
     realm: 'MY_KEYCLOAK',
     clientId: 'ANIMAL_ADOPTION_CLIENT_PUBLIC',
     urlAccount:
-      'https://keycloak.animal-adoption.com.br:8443/realms/MY_KEYCLOAK/account/',
+      'http://keycloak.animal-adoption.com.br:8080/realms/MY_KEYCLOAK/account/',
   },
 };
